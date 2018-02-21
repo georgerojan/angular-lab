40100ca1c098f8f88c92ee198a26869ad101ef20
nancy <nancyvarghese@hotmail.com>
2018-02-21 10:42:02 -0500

    completed lab 3

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+
+import { NgModule } from '@angular/core';
+
+import { MatListModule, MatCardModule, MatSlideToggleModule, MatDialogModule,         
+   MatIconModule, MatInputModule, MatSnackBarModule, MatTabsModule,           
+   MatButtonModule,MatLineModule, MatToolbarModule } from '@angular/material'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { FormsModule } from '@angular/forms'; 
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { CollectionComponent } from './collection/collection.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    CollectionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,     MatListModule,     MatTabsModule,     MatSnackBarModule,     MatDialogModule,     MatCardModule, 
+    MatIconModule,     MatSlideToggleModule,     MatButtonModule,     MatLineModule,     MatInputModule,     MatToolbarModule,     BrowserAnimationsModule 
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
